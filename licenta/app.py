8d7b48e129be8d7becd03cf0d56307785103a0b2
Laur Ciucanu <laur.ciucanu95@yahoo.com>
2019-08-18 19:22:47 +0300

    new menu

--- licenta/app.py
+++ licenta/app.py
@@ -195,6 +195,6 @@ def laborator():
 
 
 if __name__ == "__main__":
-    app.secret_key = os.urandom(12)
-    app.run(debug=True, host='0.0.0.0', port=5432)
+    # app.secret_key = os.urandom(12)
+    app.run(debug=True, host='0.0.0.0', port=5431)
     # app.run(debug=True)
