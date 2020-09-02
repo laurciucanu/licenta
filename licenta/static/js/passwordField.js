bf301387d8cb8d3dc6f6625d8ec91e7e6185d161
Laur Ciucanu <laur.ciucanu95@yahoo.com>
2020-09-02 19:45:46 +0300

    added password field toggle

--- licenta/static/js/passwordField.js
+++ licenta/static/js/passwordField.js
@@ -1,10 +1,10 @@
-$(".toggle-password").click(function() {
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#password');
 
-  $(this).toggleClass("fa-eye fa-eye-slash");
-  var input = $($(this).attr("toggle"));
-  if (input.attr("type") == "password") {
-    input.attr("type", "text");
-  } else {
-    input.attr("type", "password");
-  }
+togglePassword.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
 });
