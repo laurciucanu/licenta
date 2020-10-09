4fc7378474c3c18923bc16467faf5ddbdea7ea3b
Laur Ciucanu <laur.ciucanu95@yahoo.com>
2020-10-09 12:30:48 +0300

    update of react js depeendencies

--- licenta/app.py
+++ licenta/app.py
@@ -16,7 +16,7 @@ def home():
 
 @app.route("/index")
 def index():
-    return render_template("index.html", token="Licenta")
+    return render_template("index2.html", token="Licenta")
 
 
 @app.route("/type")
